1c913161bdd55a8200db6e0c613452146e889dcb
patrickryan <theyoungsoul@gmail.com>
2021-05-09 14:47:26 -0500

    added filename arg to hotspot-editor.py

--- hotspots/hotspot-editor.py
+++ hotspots/hotspot-editor.py
@@ -12,20 +12,22 @@ frame = None
 
 hot_spots = []
 
-def _is_point_in_circle(x,y):
+
+def _is_point_in_circle(x, y):
     for i, data in enumerate(hot_spots):
-        in_circle = (x-data[0])**2 + (y-data[1])**2 < data[2]**2
+        in_circle = (x - data[0]) ** 2 + (y - data[1]) ** 2 < data[2] ** 2
         if in_circle:
             return i
     else:
         return None
+
 
 def _draw_hotspots():
     for i, data in enumerate(hot_spots):
         cv2.circle(video_frame, (data[0], data[1]), data[2], (255, 0, 255), 1, lineType=cv2.LINE_AA)
 
 
-def draw_circle(event, x,y, flags, params):
+def draw_circle(event, x, y, flags, params):
     global video_frame
     if video_frame is not None:
         existing_hot_spot_index = _is_point_in_circle(x, y)
@@ -33,11 +35,12 @@ def draw_circle(event, x,y, flags, params):
             del hot_spots[existing_hot_spot_index]
         else:
             hot_spots.append(
-                [x,y,radius]
+                [x, y, radius]
             )
 
         video_frame = video_frame_redraw_backup.copy()
         _draw_hotspots()
+
 
 def handle_mouse_events(event, x, y, flags, params):
     global video_frame, video_frame_redraw_backup
@@ -57,12 +60,12 @@ def handle_mouse_events(event, x, y, flags, params):
             video_frame = None
 
 
-
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument("--radius", type=int, required=False, default=50,
                     help="Optional [50]: Radius size")
-    ap.add_argument("--filename", required=False, default="hotspots.csv", help="Optional[hotspots.csv] Filename to save hotspot data")
+    ap.add_argument("--filename", required=False, default="hotspots.csv",
+                    help="Optional[hotspots.csv] Filename to save hotspot data")
     ap.add_argument("--read-file", action='store_true', help="read the filename for initial hotspots")
 
     args = vars(ap.parse_args())
@@ -100,7 +103,8 @@ if __name__ == '__main__':
                     for row in csv_reader:
                         print(f'\t{row}')
                         row = list(np.float_(row))
-                        hot_spots.append([int(row[0]*frame_shape[1]), int(row[1]*frame_shape[0]), int(row[2]*(frame_shape[0]*frame_shape[1]))])
+                        hot_spots.append([int(row[0] * frame_shape[1]), int(row[1] * frame_shape[0]),
+                                          int(row[2] * (frame_shape[0] * frame_shape[1]))])
 
         k = cv2.waitKey(10) & 0xFF
         if k == ord('q'):
@@ -109,7 +113,6 @@ if __name__ == '__main__':
             save_hotspots = True
             break
 
-
     cap.release()
     cv2.destroyAllWindows()
 
@@ -117,8 +120,7 @@ if __name__ == '__main__':
         with open(filename, "w") as f:
             hotsport_writer = csv.writer(f, delimiter=',')
             for i, data in enumerate(hot_spots):
-
                 norm_x = data[0] / frame_shape[1]
                 norm_y = data[1] / frame_shape[0]
-                norm_radius = data[2] / (frame_shape[0]*frame_shape[1])
+                norm_radius = data[2] / (frame_shape[0] * frame_shape[1])
                 hotsport_writer.writerow([norm_x, norm_y, norm_radius])
